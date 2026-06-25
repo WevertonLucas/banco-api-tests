2532e0180a5abfe1f9ccae91e158dde1ee76ebf3
Weverton Lucas <w.formento@gmail.com>
2026-06-25 16:43:59 -0300

    Criei um helper de autenticação para obter o token

---
+++ helpers/autenticacao.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+
+const obterToken = async (username, senha) => {
+    const respostaLogin = await request(process.env.BASE_URL)
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({ username, senha });
+
+    return respostaLogin.body.token;
+}
+
+module.exports = {
+    obterToken
+}
